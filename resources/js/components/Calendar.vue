f00fb686653bb55bad992759281ccbd21f8ad41c
David Kjellson <david.kjellson@gmail.com>
2021-05-23 17:57:55 +0200

    Thinned the calendar a bit and placed in the center

--- resources/js/components/Calendar.vue
+++ resources/js/components/Calendar.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
+  <div class="calendar">
     <vue-scheduler :disable-dialog="true" />
   </div>
 </template>
+
+<style scoped>
+.calendar {
+  margin: 0 auto;
+  width: 75%;
+}
+</style>
