8714705df93311bae46b0a667ce41614da9b52b0
David Kjellson <david.kjellson@gmail.com>
2021-05-27 04:50:24 +0200

    New component for a flex that is just div after div after div after div after...

---
+++ resources/js/components/ButtonFlex.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="row content">
+          <div class="col-md-12">
+            <div class="button-wrapper btn-container-center">
+              <slot />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
