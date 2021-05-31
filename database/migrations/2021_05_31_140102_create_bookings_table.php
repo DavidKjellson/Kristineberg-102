6810af4c0c0ba736ac54474605e73d9e6ebbb4cd
David Kjellson <david.kjellson@gmail.com>
2021-05-31 20:30:47 +0200

    Table structure on bookings

--- database/migrations/2021_05_31_140102_create_bookings_table.php
+++ database/migrations/2021_05_31_140102_create_bookings_table.php
@@ -14,8 +14,10 @@ class CreateBookingsTable extends Migration
     public function up()
     {
         Schema::create('bookings', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('id');
+            $table->string('start');
+            $table->string('end');
+            $table->string('name');
         });
     }
 
