e9a6b2dc191d28a7c62d099408efbcdbabb61a04
David Kjellson <david.kjellson@gmail.com>
2021-05-31 23:30:35 +0200

    Booking model with actual code

--- app/Models/Bookings.php
+++ app/Models/Bookings.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Bookings extends Model
 {
     use HasFactory;
+    protected $table = 'bookings';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'start',
+        'end',
+        'name'
+    ];
 }
